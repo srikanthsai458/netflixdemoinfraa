6311381fe37a63e05520d1b7203b4bb618b64a34
srikanthsai458 <srikanthsai458@gmail.com>
2026-08-24 18:55:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ resource "aws_instance" "one" {
   ami                    = "ami-01a00762f46d584a1"
   instance_type          = "c7i-flex.large"
   key_name               = "jenkins"
-  vpc_security_group_ids = [""]
+  vpc_security_group_ids = ["sg-063802edb6a0a5bac"]
   tags = {
     Name = var.instance_names[count.index]
   }
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+  default = ["jenkins", "appserver-1", "Monitoring server"]
 }
